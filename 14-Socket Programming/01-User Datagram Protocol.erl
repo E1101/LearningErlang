084585acc2786cedc2a20a31e171865ce83fa816
Payam Naderi <naderi.payam@gmail.com>
2017-06-25 23:44:40 +0430

    socket

---
+++ 14-Socket Programming/01-User Datagram Protocol.erl
@@ -0,0 +1,34 @@
+
+% > To open a socket, on both the client and the server side
+
+gen_udp:open(Port)
+gen_udp:open(Port, OptionList)
+% {ok, Socket} | {error, Reason}
+
+% - Port
+%   an integer denoting the listening port number of the socket.
+% - OptionList
+%   list: Forwards all messages in the packet as a list of integers *default
+%   binary: Forwards all messages in the packet as a binary.
+%   {header, Size}: used if packets are being received as binaries. It splits the message into a
+%                   list of size Size , the header, and the message (a binary).
+%   {active, true}: ensures that all the messages received from the socket are
+%                   forwarded to the process that owns the socket as Erlang messages of the form
+%                   {udp, Socket, IP, PortNo, Packet}. *default
+%   {active, false}: Sets the socket to passive mode. Instead of being sent, messages from the socket
+%                    have to be retrieved using the gen_udp:recv/2 and gen_udp:recv/3 calls.
+%   {active, once}: send the first message it receives to the socket, but subsequent messages have
+%                   to be retrieved using the recv functions.
+%   {ip, ip_address()}: used when opening a socket on a computer that has several network interfaces defined.
+%   inet6: set up the socket for IPv6.
+
+
+% > closes the socket and frees the port number allocated to it
+
+gen_udp:close(Socket)
+
+
+% > send messages
+
+gen_udp:send(Socket, Address, Port, Packet)
+
